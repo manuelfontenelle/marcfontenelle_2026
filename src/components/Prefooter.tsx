dc839ce0cec3a686ed65af9f68c2e8dddb1b3143
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2026-07-07 00:06:10 +0200

    fix2

--- src/components/Prefooter.tsx
+++ src/components/Prefooter.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import Link from "next/link";
+import { useState } from "react";
 
 interface PrefooterProps {
 	variant?: "works" | "default";
 }
 
 export default function Prefooter({ variant = "default" }: PrefooterProps) {
+	const [tooltip, setTooltip] = useState("Copier l'email !");
+	const email = "marc.fontenelle.pro@gmail.com";
+
+	const copyEmail = async () => {
+		try {
+			await navigator.clipboard.writeText(email);
+			setTooltip("Copié !");
+		} catch {
+			setTooltip("Copier l'email !");
+		}
+	};
+
 	return (
 		<div className={`block-prefooter ${variant === "works" ? "works" : ""}`}>
 			<div className="first-block">
@@ -14,9 +26,22 @@ export default function Prefooter({ variant = "default" }: PrefooterProps) {
 					<div className="first-block__block-top__first">
 						Vous avez un projet ?<span>Parlons-en</span>
 					</div>
-					<Link href="/contact" className="first-block__block-top__second">
+					<div className="first-block__block-top__second">
 						Envoyez-moi<span>Un message</span>
-					</Link>
+					</div>
+				</div>
+				<div
+					className="first-block__block-mail"
+					id="containerMailToCopy"
+					onClick={copyEmail}
+					onKeyDown={(e) => e.key === "Enter" && copyEmail()}
+					role="button"
+					tabIndex={0}
+				>
+					<span className="tooltiptext" id="tooltiptext">
+						{tooltip}
+					</span>
+					<div id="mailToCopy">{email}</div>
 				</div>
 			</div>
 			<img
